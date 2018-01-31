985c7b7da5b26a5bcef911c932d031139b057b4b
Pedro Chiesa <p_chiesa@hotmail.com>
2018-01-31 00:18:21 -0200

    Created .dll to allow update of Store names and hashes

---
+++ HashMaker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WebAPI.Services;
+
+namespace HashMaker
+{
+    class Program
+    {
+        static HashStringService hash = new HashStringService();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type string you want to hash:");
+            var typedString = Console.ReadLine();
+            Console.WriteLine($"Typed string: {typedString}");
+
+            var hashedString = hash.Hash(typedString);
+
+            Console.WriteLine($"Hashed string: {hashedString}");
+
+            Console.WriteLine("Type any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
